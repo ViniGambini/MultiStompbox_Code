e6ecd2101d2d80aafeccc58529e479c316d5515c
ViniGambini <vincentgoulet15@gmail.com>
2024-03-07 13:05:30 -0500

    ajout plein truc dont etat select bloc vol complet

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #define POT_0 0x00 // adresse interne du pot 0
 #define POT_1 0x10 // adresse interne du pot 1
 
-#define increment_pot 2 // nombre de pourcentage par clic de l'encodeur
+#define increment_pot 5 // nombre de pourcentage par clic de l'encodeur
 
 #define WIDTH screen.width()   // 240
 #define HEIGHT screen.height() // 320
@@ -37,6 +37,10 @@ enum etat
   select_bloc_nom,
   change_nom,
   select_bloc_vol,
+  select_vol,
+  change_val_vol,
+  select_mix,
+  change_val_mix,
   select_bloc_ctrl,
   select_bloc_para
 
@@ -138,9 +142,9 @@ void menu_princ(void)
   screen.drawFastHLine(0, offset_pot_ctrl + hauteur_texte_2 * 3 + hauteur_texte_3 * 3 + distance_entre_ligne * 6, WIDTH, couleur_ligne);
 
   // Print le bloc paramètre
-  screen.setTextSize(3);
+  /*screen.setTextSize(3);
   screen.setCursor(offset_x, offset_parametre);
-  screen.print("Parametres");
+  screen.print("Parametres");*/
 }
 
 void menu_change_nom(void)
@@ -159,6 +163,8 @@ void clear_screen(void)
   screen.fillScreen(couleur_fond);
   screen.drawRect(0, 0, WIDTH, HEIGHT, couleur_ligne);
 }
+
+// Les fonctions suivante permettent de mettre en évidence certains aspects de l'affichage
 
 void highlight_bloc_nom(void)
 {
@@ -194,6 +200,115 @@ void highlight_bloc_para(void)
 void unhighlight_bloc_para(void)
 {
   screen.drawRect(0, offset_parametre - distance_entre_ligne, WIDTH, HEIGHT - offset_parametre + distance_entre_ligne, couleur_ligne);
+}
+
+void highlight_texte_vol(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(WIDTH / 2 - 75, offset_pot_vol);
+  screen.print("Vol");
+}
+void unhighlight_texte_vol(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(WIDTH / 2 - 75, offset_pot_vol);
+  screen.print("Vol");
+}
+
+void highlight_texte_mix(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(WIDTH / 2 + 25, offset_pot_vol);
+  screen.print("Mix");
+}
+void unhighlight_texte_mix(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(WIDTH / 2 + 25, offset_pot_vol);
+  screen.print("Mix");
+}
+
+void highlight_texte_ctrl1(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(offset_x, offset_pot_ctrl);
+  screen.print(effets.get_nom_ctrl1(effet_actif));
+}
+void unhighlight_texte_ctrl1(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(offset_x, offset_pot_ctrl);
+  screen.print(effets.get_nom_ctrl1(effet_actif));
+}
+
+void highlight_texte_ctrl2(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(offset_x, offset_pot_ctrl + hauteur_texte_2 + hauteur_texte_3 + distance_entre_ligne * 2);
+  screen.print(effets.get_nom_ctrl2(effet_actif));
+}
+void unhighlight_texte_ctrl2(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(offset_x, offset_pot_ctrl + hauteur_texte_2 + hauteur_texte_3 + distance_entre_ligne * 2);
+  screen.print(effets.get_nom_ctrl2(effet_actif));
+}
+
+void highlight_texte_ctrl3(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(offset_x, offset_pot_ctrl + hauteur_texte_2 * 2 + hauteur_texte_3 * 2 + distance_entre_ligne * 4);
+  screen.print(effets.get_nom_ctrl3(effet_actif));
+}
+void unhighlight_texte_ctrl3(void)
+{
+  screen.setTextSize(3);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(offset_x, offset_pot_ctrl + hauteur_texte_2 * 2 + hauteur_texte_3 * 2 + distance_entre_ligne * 4);
+  screen.print(effets.get_nom_ctrl3(effet_actif));
+}
+
+void highlight_pourcent_vol(void)
+{
+  screen.setTextSize(2);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(WIDTH / 2 - 65, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+  screen.print(effets.lire_val_pot_vol());
+  screen.print("%");
+}
+void unhighlight_pourcent_vol(void)
+{
+  screen.setTextSize(2);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(WIDTH / 2 - 65, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+  screen.print(effets.lire_val_pot_vol());
+  screen.print("%");
+}
+
+void highlight_pourcent_mix(void)
+{
+  screen.setTextSize(2);
+  screen.setTextColor(couleur_select);
+  screen.setCursor(WIDTH / 2 + 35, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+  screen.print(effets.lire_val_pot(effet_actif, Mix));
+  screen.print("%");
+}
+void unhighlight_pourcent_mix(void)
+{
+  screen.setTextSize(2);
+  screen.setTextColor(couleur_normal);
+  screen.setCursor(WIDTH / 2 + 35, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+  screen.print(effets.lire_val_pot(effet_actif, Mix));
+  screen.print("%");
 }
 
 //----------------------------------------------//
@@ -250,7 +365,6 @@ void loop()
         highlight_bloc_vol();
       }
     }
-
     // Lecture du bouton
     btnState = digitalRead(IO_SW_ENC);
     if (btnState == LOW)
@@ -286,7 +400,6 @@ void loop()
       }
       menu_change_nom();
     }
-
     // Lecture du bouton
     btnState = digitalRead(IO_SW_ENC);
     if (btnState == LOW)
@@ -317,6 +430,136 @@ void loop()
         highlight_bloc_ctrl();
       }
     }
+    // Lecture du bouton
+    btnState = digitalRead(IO_SW_ENC);
+    if (btnState == LOW)
+    {
+      if (millis() - lastButtonPress > 50)
+      {
+        etat_affichage = select_vol;
+        unhighlight_bloc_vol();
+        highlight_texte_vol();
+      }
+      lastButtonPress = millis();
+    }
+    break;
+
+  case select_vol:
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+      if (digitalRead(IO_S2_ENC) != currentStateCLK)
+      { // CCW
+      }
+      else
+      { // CW
+        etat_affichage = select_mix;
+        unhighlight_texte_vol();
+        highlight_texte_mix();
+      }
+    }
+    // Lecture du bouton
+    btnState = digitalRead(IO_SW_ENC);
+    if (btnState == LOW)
+    {
+      if (millis() - lastButtonPress > 50)
+      {
+        etat_affichage = change_val_vol;
+        unhighlight_texte_vol();
+        highlight_pourcent_vol();
+      }
+      lastButtonPress = millis();
+    }
+    break;
+
+  case change_val_vol:
+    // Lecture de l'encodeur
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+      screen.fillRect(WIDTH / 2 - 65, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne, 50, hauteur_texte_2, couleur_fond);
+      if (digitalRead(IO_S2_ENC) != currentStateCLK)
+      { // CCW
+        screen.setCursor(WIDTH / 2 - 65, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+        screen.print(effets.reduire_val_pot_vol(increment_pot));
+        screen.print("%");
+      }
+      else
+      { // CW
+        screen.setCursor(WIDTH / 2 - 65, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+        screen.print(effets.augmenter_val_pot_vol(increment_pot));
+        screen.print("%");
+      }
+    }
+    // Lecture du bouton
+    btnState = digitalRead(IO_SW_ENC);
+    if (btnState == LOW)
+    {
+      if (millis() - lastButtonPress > 50)
+      {
+        etat_affichage = select_bloc_vol;
+        unhighlight_pourcent_vol();
+        highlight_bloc_vol();
+      }
+      lastButtonPress = millis();
+    }
+    break;
+
+  case select_mix:
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+      if (digitalRead(IO_S2_ENC) != currentStateCLK)
+      { // CCW
+        etat_affichage = select_vol;
+        unhighlight_texte_mix();
+        highlight_texte_vol();
+      }
+      else
+      { // CW
+      }
+    }
+    // Lecture du bouton
+    btnState = digitalRead(IO_SW_ENC);
+    if (btnState == LOW)
+    {
+      if (millis() - lastButtonPress > 50)
+      {
+        etat_affichage = change_val_mix;
+        unhighlight_texte_mix();
+        highlight_pourcent_mix();
+      }
+      lastButtonPress = millis();
+    }
+    break;
+
+  case change_val_mix:
+    // Lecture de l'encodeur
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+      screen.fillRect(WIDTH / 2 + 35, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne, 50, hauteur_texte_2, couleur_fond);
+      if (digitalRead(IO_S2_ENC) != currentStateCLK)
+      { // CCW
+        screen.setCursor(WIDTH / 2 + 35, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+        screen.print(effets.reduire_val_pot(effet_actif, Mix, increment_pot));
+        screen.print("%");
+      }
+      else
+      { // CW
+        screen.setCursor(WIDTH / 2 + 35, offset_pot_vol + hauteur_texte_3 + distance_entre_ligne);
+        screen.print(effets.augmenter_val_pot(effet_actif, Mix, increment_pot));
+        screen.print("%");
+      }
+    }
+    // Lecture du bouton
+    btnState = digitalRead(IO_SW_ENC);
+    if (btnState == LOW)
+    {
+      if (millis() - lastButtonPress > 50)
+      {
+        etat_affichage = select_bloc_vol;
+        unhighlight_pourcent_mix();
+        highlight_bloc_vol();
+      }
+      lastButtonPress = millis();
+    }
     break;
 
   case select_bloc_ctrl:
@@ -330,14 +573,14 @@ void loop()
       }
       else
       { // CW
-        etat_affichage = select_bloc_para;
+        /*etat_affichage = select_bloc_para;
         unhighlight_bloc_ctrl();
-        highlight_bloc_para();
+        highlight_bloc_para();*/
       }
     }
     break;
 
-    case select_bloc_para:
+  case select_bloc_para:
     if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
     {
       if (digitalRead(IO_S2_ENC) != currentStateCLK)
